ae171f65beb0a73684fc3182c994b69795a17bea
hin1115 <hoin1115@gmail.com>
2021-05-07 11:28:57 +0900

    bug fixed

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sys
 import inferer
 from config import parse
 
-# os.environ['CUDA_VISIBLE_DEVICES']='1'
+os.environ['CUDA_VISIBLE_DEVICES']='1'
 # os.environ['CUDA_VISIBLE_DEVICES']='0'
 config_path = './param_config.yml'
 config = parse(config_path)
